9ceb1445c7abce0f6897a24423c3d0562b88da5c
Arunprasat Selvaraj <smarun16@gmail.com>
2019-07-17 05:09:36 +0100

    Initial Commit

---
+++ JourneyTracker/Controller/JourneyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  JourneyTracker
+//
+//  Created by Arunprasat Selvaraj on 15/07/2019.
+//  Copyright © 2019 Arunprasat Selvaraj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JourneyTracker/Controller/TrackingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JourneyTracker
+//
+//  Created by Arunprasat Selvaraj on 15/07/2019.
+//  Copyright © 2019 Arunprasat Selvaraj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
